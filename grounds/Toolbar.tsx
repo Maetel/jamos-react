89a3ef6d404e428ebc8ea50cd533100d1e469757
jam_mac <iamjam4944@gmail.com>
2022-06-23 19:52:30 +0900

    add toolbar prototype

--- grounds/Toolbar.tsx
+++ grounds/Toolbar.tsx
@@ -1,11 +1,24 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useAppSelector } from "../app/hooks";
+import ProcMgr from "../features/procmgr/ProcMgr";
 import SetMgr from "../features/settings/SetMgr";
 import styles from "../styles/Toolbar.module.css";
 
+export interface CollapsibleMenu {
+  title: string;
+  items: CollapsibleItem[];
+}
+export interface CollapsibleItem {
+  name: string;
+  disabled?: boolean;
+  callback?: () => void;
+  separator?: boolean;
+}
+
 function ToolbarClock(props) {
-  const [time, setTime] = useState(new Date().toLocaleString());
+  const [time, setTime] = useState("");
   useEffect(() => {
+    setTime(new Date().toLocaleString());
     const id = setInterval(() => {
       setTime(() => new Date().toLocaleString());
     }, 1000);
@@ -16,9 +29,123 @@ function ToolbarClock(props) {
   return <span className={styles.toolbarClock}>{time}</span>;
 }
 
+function MenuItem(props) {
+  const item: CollapsibleItem = props.item;
+  const disabled = item.disabled;
+  const colors = SetMgr.getInstance().themeReadable(useAppSelector).colors;
+  const [hovered, setHovered] = useState(false);
+  const buildStyle = () => {
+    const retval = {
+      color: colors["2"],
+      backgroundColor: colors["1"],
+    };
+    if (disabled) {
+      // retval.color = colors["3"];
+      retval["textDecoration"] = "underline";
+      return retval;
+    }
+    if (hovered) {
+      retval.color = colors["1"];
+      retval.backgroundColor = colors["2"] + "aa";
+    }
+    if (item.separator) {
+      retval["borderBottom"] = `1px solid ${colors["2"]}`;
+    }
+    return retval;
+  };
+  const style = buildStyle();
+  return (
+    <span
+      className={styles.menuitem}
+      onClick={(e) => {
+        item.callback?.();
+        props.uncollapse?.();
+      }}
+      onMouseEnter={() => {
+        setHovered(true);
+      }}
+      onMouseLeave={() => {
+        setHovered(false);
+      }}
+      style={style}
+    >
+      {item.name}
+    </span>
+  );
+}
+
+function CollapsibleMenu(props) {
+  const menu: CollapsibleMenu = props.menu;
+  const colors = SetMgr.getInstance().themeReadable(useAppSelector).colors;
+  const collMenuElem = useRef(null);
+  const [collActive, setCollActive] = useState(false);
+  const isActive = collActive && props.clicked === props.id;
+  const itemClassName = isActive ? styles.active : "";
+
+  const [posX, setPosX] = useState(undefined);
+  useEffect(() => {
+    // setPosX(getComputedStyle(collMenuElem.current).left);
+  }, []);
+  const onTitleClick = (e) => {
+    console.log("Ontitleclick x :", `${e.target.getBoundingClientRect().x}px`);
+    setPosX(`${e.target.getBoundingClientRect().x}px`);
+    setCollActive(true);
+    props.menuActivate?.();
+  };
+
+  const items: CollapsibleItem[] = menu.items ?? [
+    { name: "No item", disabled: true },
+  ];
+
+  const buildItemsStyle = () => {
+    const retval = {
+      left: posX,
+      // width: 200,
+      // height: `${Math.round(items.length * 1.6)}rem`,
+      zIndex: 1002,
+      backgroundColor: colors["1"],
+      color: colors["2"],
+      boxShadow: `1px 1px 20px ${colors["2"]}aa`,
+    };
+    retval["position"] = "fixed";
+    return retval;
+  };
+  const buildTitleStyle = () => {
+    return {
+      color: colors[isActive ? "1" : "2"],
+      backgroundColor: `${colors[isActive ? "2" : "1"]}aa`,
+    };
+  };
+  const itemsStyle = buildItemsStyle();
+  const titleStyle = buildTitleStyle();
+
+  return (
+    <>
+      <span
+        className={`${styles.collTitle}`}
+        onClick={onTitleClick}
+        ref={collMenuElem}
+        style={titleStyle}
+      >
+        {menu.title}
+      </span>
+      <div className={`${styles.items} ${itemClassName}`} style={itemsStyle}>
+        {items.map((item, i) => (
+          <MenuItem
+            key={i}
+            item={item}
+            uncollapse={props.uncollapse}
+          ></MenuItem>
+        ))}
+      </div>
+    </>
+  );
+}
+
 export default function Toolbar(props) {
-  const [active, setActive] = useState(props.show);
-  const show: boolean = props.show || active;
+  const [hovered, setHovered] = useState(props.show);
+  const [clicked, setClicked] = useState(null);
+  const show: boolean = props.show || hovered || clicked !== null;
   const className = show ? styles.active : "";
   const colors = SetMgr.getInstance().themeReadable(useAppSelector).colors;
 
@@ -30,21 +157,85 @@ export default function Toolbar(props) {
   };
   const containerStyle = buildContainerStyle();
 
+  const procmgr = ProcMgr.getInstance();
+  const collapsibles: CollapsibleMenu[] = [
+    {
+      title: "   🍞   ",
+      items: [
+        {
+          name: "About",
+          callback: () => {
+            procmgr.add("terminal");
+          },
+          separator: true,
+        },
+        {
+          name: "Settings",
+          callback: () => {
+            procmgr.add("settings");
+          },
+        },
+        {
+          name: "AppStore",
+          callback: () => {
+            procmgr.add("bakery");
+          },
+          separator: true,
+        },
+        {
+          name: "Terminal",
+          callback: () => {
+            procmgr.add("terminal");
+          },
+        },
+        {
+          name: "Close all windows",
+          callback: () => {
+            procmgr.killAll();
+          },
+        },
+      ],
+    },
+  ];
+
+  const uncollapse = (e) => {
+    setHovered(false);
+    setClicked(null);
+  };
+
   return (
-    <div
-      className={`${styles.container} ${className}`}
-      style={containerStyle}
-      onMouseEnter={(e) => {
-        setActive(true);
-      }}
-      onMouseLeave={(e) => {
-        setActive(false);
-      }}
-    >
-      <span className="left">JamOS Toolbar</span>
-      <span className="right">
-        <ToolbarClock></ToolbarClock>
-      </span>
-    </div>
+    <>
+      <div
+        className={`${styles.container} ${className}`}
+        style={containerStyle}
+        onMouseEnter={(e) => {
+          setHovered(true);
+        }}
+        onMouseLeave={(e) => {
+          setHovered(false);
+        }}
+      >
+        <span className={styles.left}>
+          {collapsibles.map((coll, i) => {
+            return (
+              <CollapsibleMenu
+                menu={coll}
+                id={i}
+                key={i}
+                menuActivate={() => {
+                  setClicked(i);
+                }}
+                clicked={clicked}
+                uncollapse={uncollapse}
+              />
+            );
+          })}
+        </span>
+        <span className={styles.right}>
+          <ToolbarClock></ToolbarClock>
+        </span>
+      </div>
+      <div className={`${styles.bg} ${className}`} onClick={uncollapse}></div>
+    </>
   );
 }
